3ff4ebca87d4ab8c0122c33affc2912a4174f794
m1sha <m1sha@live.ru>
2025-12-21 22:23:34 +0700

    search for textbox

--- src/components/text-box/text-box.vue
+++ src/components/text-box/text-box.vue
@@ -4,23 +4,34 @@ import { uuidv4 } from '../../utils/uuidv4'
 import { type LabelProps } from '../label-props'
 
 const modelValue = defineModel<T>({ required: true })
-defineProps<{ type?: 'text' | 'password' | 'number' } & LabelProps>()
+defineProps<{ type?: 'text' | 'password' | 'number', search?: boolean, placeholder?: string } & LabelProps>()
 const controlId = ref('tb_' + uuidv4())
 </script>
 
 <template>
-  <div class="textbox">
+  <div class="vue3-uui__textbox">
     <label :for="controlId" v-if="caption">{{ caption }}</label>
-    <input :id="controlId" :type="type? type :'text'" v-model="modelValue"  />
+    <div class="vue3-uui__input-wrapper">
+      <input :id="controlId" :type="type? type :'text'" v-model="modelValue" :placeholder="placeholder"  :style="{ paddingLeft: search ? '18px' : '4px' }" />
+      <i v-if="search" class="fa fa-magnifying-glass"></i>
+    </div>
   </div>
 </template>
 
 <style lang="sass">
 @use '../../assets/_base-control' as control
 
-.textbox
+.vue3-uui__textbox
   @include control.flex-container()
-
-  input
-    width: auto
+  .vue3-uui__input-wrapper
+    position: relative
+    input
+      width: auto
+    i
+      position: absolute
+      z-index: 1
+      left: 0
+      font-size: 0.8em
+      top: 8px
+      color: #888
 </style>
