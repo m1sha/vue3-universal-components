49f35687506d950ca63867a33e7e1e83103869c3
m1sha <m1sha@live.ru>
2026-06-09 20:08:34 +0700

    text-box stopPropagation

--- src/components/text-box/text-box.vue
+++ src/components/text-box/text-box.vue
@@ -4,15 +4,27 @@ import { uuidv4 } from '../../utils/uuidv4'
 import { type LabelProps } from '../label-props'
 
 const modelValue = defineModel<T>({ required: true })
-defineProps<{ type?: 'text' | 'password' | 'number', search?: boolean, placeholder?: string } & LabelProps>()
+const props = defineProps<{ type?: 'text' | 'password' | 'number' | 'int', search?: boolean, placeholder?: string } & LabelProps>()
 const controlId = ref('tb_' + uuidv4())
+const onInput = (e: Event) => {
+  const value =  (e.target as HTMLInputElement).value as T
+  const val = 
+    props.type == 'number' 
+    ? parseFloat(value as string) as T 
+    : props.type == 'int' 
+    ? parseInt(value as string) as T 
+    : value
+
+  if((props.type == 'number'  ||  props.type == 'int') && isNaN(val as number)) return
+  modelValue.value = val
+}
 </script>
 
 <template>
   <div class="vue3-uui__textbox">
     <label :for="controlId" v-if="caption">{{ caption }}</label>
     <div class="vue3-uui__input-wrapper">
-      <input :id="controlId" :type="type? type :'text'" v-model="modelValue" :placeholder="placeholder"  :style="{ paddingLeft: search ? '18px' : '4px' }" />
+      <input :id="controlId" :type="type? type :'text'" :value="modelValue" :placeholder="placeholder"  :style="{ paddingLeft: search ? '18px' : '4px' }" @keydown.stop="onInput($event)" />
       <i v-if="search" class="fa fa-magnifying-glass"></i>
     </div>
   </div>
